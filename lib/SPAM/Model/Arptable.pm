cb52fe13133fa7488a2d23c2d231ac993de730b5
Borek Lupomesky <borek.lupomesky@vodafone.com>
2023-01-17 10:49:41 +0100

    Make upsert the only supported operation on backend db
    Separate insert and update primitives are not needed anything, so we only
    implement the 'insert_or_update' method.  This method also uses ON
    CONFLICT clause so that there is no need to load previous state from the
    db.

--- lib/SPAM/Model/Arptable.pm
+++ lib/SPAM/Model/Arptable.pm
@@ -35,40 +35,18 @@ sub _build__arpdb ($self)
 sub get_arp ($self, $ip) { $self->_arpdb->{$ip} // undef }
 
 #-------------------------------------------------------------------------------
-# insert single ARP entry into backend database
-sub insert ($self, $dbh, %data)
-{
-  return $dbh->do(
-    'INSERT INTO arptable (source,mac,ip,lastchk,dnsname) ' .
-    'VALUES ( ?,?,?,current_timestamp,?',
-    undef,
-    $data{source}, $data{mac}, $data{ip}, $data{dnsname}
-  );
-}
-
-#-------------------------------------------------------------------------------
-# update single ARP entry in backend database
-sub update ($self, $dbh, %data)
-{
-  return $dbh->do(
-    'UPDATE arptable ' .
-    'SET mac = ?, lastchk = current_timestamp, dnsname = ? ' .
-    'WHERE source = ? AND ip  = ?',
-    undef,
-    $data{mac}, $data{dnsname}, $data{source}, $data{ip}
-  )
-}
-
-#-------------------------------------------------------------------------------
 # insert or update single ARP entry in backend database depending on the state
 # of the instance
 sub insert_or_update($self, $dbh, %data)
 {
-  if($self->get_arp($data{ip})) {
-    $self->update($dbh, %data);
-  } else {
-    $self->insert($dbh, %data);
-  }
+  return $dbh->do(
+    'INSERT INTO arptable2 (source,mac,ip,lastchk,dnsname) ' .
+    'VALUES ( ?,?,?,current_timestamp,? ) ' .
+    'ON CONFLICT (source, ip) DO ' .
+    'UPDATE SET lastchk = current_timestamp, mac = ?',
+    undef,
+    $data{source}, $data{mac}, $data{ip}, $data{dnsname}, $data{mac}
+  );
 }
 
 1;
