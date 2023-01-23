8efb2e63b488ec0a5c4a4d8138d4cdadff6d1eec
Borek Lupomesky <borek.lupomesky@vodafone.com>
2023-01-23 14:55:28 +0100

    Switch reading of arptable to the new code
    This significantly reworks the framework of host polling code.  This
    commit does not update frontend yet.

--- spam.pl
+++ spam.pl
@@ -27,84 +27,15 @@ use SPAM::SNMP;
 use SPAM::Cmdline;
 use SPAM::Config;
 use SPAM::Host;
-use SPAM::DbTransaction;
 
 $| = 1;
-
-#-------------------------------------------------------------------------------
-# This function updates arptable in backend database
-sub sql_arptable_update ($arptable)
-{
-  my $cfg = SPAM::Config->instance;
-  my $dbh = $cfg->get_dbi_handle('spam');
-  my %arp_current;
-  my ($mac, $ret, $q);
-
-  # get new transaction instance
-  my $tx = SPAM::DbTransaction->new;
-
-  # ensure database connection
-  return 'Cannot connect to database (spam)' unless ref $dbh;
-
-  # query current state
-  my $sth = $dbh->prepare('SELECT mac FROM arptable');
-  $sth->execute
-    || return 'Database query failed (spam,' . $sth->errstr . ')';
-  while(($mac) = $sth->fetchrow_array) {
-    $arp_current{$mac} = 0;
-  }
-
-  # gather update plan
-  foreach $mac (keys %$arptable) {
-    my $aux = strftime("%c", localtime());
-    if(exists $arp_current{lc($mac)}) {
-
-      # update
-      $tx->add(
-        q{UPDATE arptable SET ip = ?, lastchk = ? WHERE mac = ?},
-        $arptable->{$mac}, $aux, $mac
-      );
-
-    } else {
-
-      # insert
-      {
-        my $iaddr = inet_aton($arptable->{$mac});
-        my $dnsname = gethostbyaddr($iaddr, AF_INET);
-
-        my @fields = qw(mac ip lastchk);
-        my @bind = (
-          $mac, $arptable->{$mac},
-          $aux
-        );
-
-        if($dnsname) {
-          push(@fields, 'dnsname');
-          push(@bind, lc($dnsname));
-        }
-
-        $tx->add(
-          sprintf(
-            q{INSERT INTO arptable ( %s ) VALUES ( %s )},
-            join(',', @fields),
-            join(',', (('?') x @fields))
-          ),
-          @bind
-        );
-      }
-    }
-  }
-
-  # send update to the database
-  return $tx->commit;
-}
 
 #-------------------------------------------------------------------------------
 # This function finds another task to be scheduled for run.
 sub schedule_task ($work_list)
 {
   die unless ref $work_list;
-  foreach (@$work_list) { return $_ unless defined $_->[2] }
+  foreach (@$work_list) { return $_ unless defined $_->[1] }
   return undef;
 }
 
@@ -113,25 +44,13 @@ sub schedule_task ($work_list)
 sub clear_task_by_pid ($work_list, $pid)
 {
   foreach (@$work_list) {
-    if($_->[2] == $pid) {
-      $_->[2] = 0;
+    if($_->[1] == $pid) {
+      $_->[1] = 0;
       return $_;
     }
   }
   return undef;
 }
-
-
-#================  #  ======================================================
-#===                         ===============================================
-#===  ## #   ###  ##  ####   ===============================================
-#===  # # #     #  #  #   #  ===============================================
-#===  # # #  ####  #  #   #  ===============================================
-#===  # # # #   #  #  #   #  ===============================================
-#===  # # #  #### ### #   #  ===============================================
-#===                         ===============================================
-#===========================================================================
-
 
 # display title
 tty_message(<<EOHD);
@@ -218,121 +137,125 @@ try {
     }
 	}
 
-	# close connection to ondb database
-	tty_message("[main] Closing connection to ondb database\n");
-	$cfg->close_dbi_handle('ondb');
-
-	# create work list of hosts that are to be processed
-	my @work_list;
-	my $poll_hosts_re = $cmd->hostre();
-	foreach my $host (sort keys $cfg->hosts->%*) {
-    if(
-      (
-        $cmd->hosts->@* &&
-        grep { lc($host) eq lc($_); } $cmd->hosts->@*
-      ) || (
-        $poll_hosts_re &&
-        $host =~ /$poll_hosts_re/i
-      ) || (
-        !$cmd->hosts->@* && !defined($poll_hosts_re)
-      )
-    ) {
-      push(@work_list, [ 'host', $host, undef ]);
-    }
-	}
+  # create work list of hosts that are to be polled for data
+  my $poll_hosts_re = $cmd->hostre;
+  my $hosts = $cfg->worklist(sub ($h, $v) {
+    my $rv = $v;
+    # filtering by host enumeration
+    $rv = undef if $cmd->hosts->@* && !(grep { lc($h) eq lc($_); } $cmd->hosts->@*);
+    # filtering by host regular expression
+    $rv = undef if $poll_hosts_re && $h !~ /$poll_hosts_re/i;
+    # remove arptable
+    $rv = [ grep { $_ ne 'arpsource' } @$rv ] unless $cmd->arptable;
+    # finish
+    return $rv;
+  });
 
   # --force-host processing
   if($cmd->has_forcehost) {
     # if --force-host is in effect and neither --host or --hostre are present
     # the loaded list of hosts is dropped as only forced host will be processed;
     # FIXME: in that case loading of the host list is unnecessary
-    @work_list = () unless $cmd->has_hostre || $cmd->hosts->@*;
+    $hosts = {} unless $cmd->has_hostre || $cmd->hosts->@*;
     # add forced hosts to worklist unless it already is in it
     foreach my $fhost ($cmd->forcehost->@*) {
-      push(@work_list, [ 'host', $fhost, undef ])
-      unless grep { $_->[0] eq 'host' && $_->[1] eq $fhost } @work_list;
+      $hosts->{$fhost} = [ 'switch '];
     }
   }
 
-	tty_message("[main] %d hosts scheduled to be processed\n", scalar(@work_list));
-
-	# add arptable task to the work list
-  push(@work_list, [ 'arp', undef, undef ]) if $cmd->arptable;
+	tty_message(
+    "[main] %d hosts scheduled to be processed\n", scalar(keys %$hosts)
+  );
 
   # --worklist option selected, only print the worklist and finish
   if($cmd->list_worklist) {
     printf("\nFollowing host would be scheduled for polling\n");
     printf(  "=============================================\n");
-    for my $we (@work_list) {
-      printf("%s %s\n",@{$we}[0..1]);
+    for my $h (sort keys %$hosts) {
+      print scalar(grep { $_ eq 'switch' } $hosts->{$h}->@*) ? 'S' : ' ';
+      print scalar(grep { $_ eq 'arpsource' } $hosts->{$h}->@*) ? 'A' : ' ';
+      printf("  %s\n", $h);
     }
-    print "\n";
+    printf("\n%d hosts scheduled\n\n", scalar(keys %$hosts));
     die "OK\n";
+  }
+
+  # host instance creation helper
+  my $ht = sub ($h, $roles) {
+    SPAM::Host->new(
+      name => $h, roles => $roles, mesg => sub ($s, @arg) {
+        tty_message("$s\n", @arg);
+      }
+    );
+  };
+
+  # convert current list of hosts to worklist array
+  my @work_list;
+  foreach my $h (keys %$hosts) {
+    push(@work_list, [ $ht->($h, $hosts->{$h}), undef ]);
   }
 
 	# loop through all tasks
 	my $tasks_cur = 0;
 
 	while(defined(my $task = schedule_task(\@work_list))) {
-    my $host = $task->[1];
+    my $hi = $task->[0];
     my $pid = fork();
-	  if($pid == -1) {
-	    die "Cannot fork() new process";
-	  }
+
+    # failed to fork
+    if($pid == -1) {
+      die "Cannot fork() new process";
+    }
 
     # parent process
     elsif($pid > 0) {
       $tasks_cur++;
-      $task->[2] = $pid;
-      tty_message("[main] Child $host (pid $pid) started\n");
+      $task->[1] = $pid;
+      tty_message("[main] Child %s (pid %d) started\n", $hi->name, $pid);
       if($tasks_cur >= $cmd->tasks) {
         my $cpid;
         if(($cpid = wait()) != -1) {
           $tasks_cur--;
           my $ctask = clear_task_by_pid(\@work_list, $cpid);
-          tty_message(
-            "[main] Child %s reaped\n",
-            $ctask->[0] eq 'host' ? $ctask->[1] : 'arptable'
-          );
+          tty_message("[main] Child %s reaped\n", $ctask->[0]->name);
         } else {
-          die "Assertion failed! No children running.";
+          die 'Assertion failed! No children running.';
         }
       }
     }
 
     # child process
     else {
-      if($task->[0] eq 'host') {
-        tty_message("[$host] Processing started\n");
+      tty_message(
+        "[%s] Processing started (%s)\n", $hi->name, join(',', $hi->roles->@*)
+      );
 
-        try {
+      try {
 
-          my $hi = SPAM::Host->new(name => $host, mesg => sub ($s, @arg) {
-            tty_message("$s\n", @arg);
-          });
+        # display SNMP profile
+        tty_message("[%s] SNMP profile: %s\n", $hi->name, $hi->snmp_profile);
 
-          # display SNMP profile
-          tty_message("[$host] SNMP profile: %s\n", $hi->snmp_profile);
+        # perform host poll
+        $hi->poll($cmd->mactable, $cmd->hostinfo);
 
-          # perform host poll
-          $hi->poll($cmd->mactable, $cmd->hostinfo);
+        # only hostinfo, no more processing
+        die "hostinfo only\n" if $cmd->hostinfo;
 
-          # only hostinfo, no more processing
-          die "hostinfo only\n" if $cmd->hostinfo;
+        # find changes and update status table
+        $hi->update_db;
 
-          # find changes and update status table
-          $hi->update_db;
+        if($hi->has_role('switch')) {
 
           # update swstat table
-          tty_message("[$host] Updating swstat table (started)\n");
+          tty_message("[%s] Updating swstat table (started)\n", $hi->name);
           $hi->swstat->update($hi->snmp, $hi->port_stats);
-          tty_message("[$host] Updating swstat table (finished)\n");
+          tty_message("[%s] Updating swstat table (finished)\n", $hi->name);
 
           # update mactable
           if($cmd->mactable()) {
-            tty_message("[$host] Updating mactable (started)\n");
+            tty_message("[%s] Updating mactable (started)\n", $hi->name);
             $hi->update_mactable;
-            tty_message("[$host] Updating mactable (finished)\n");
+            tty_message("[%s] Updating mactable (finished)\n", $hi->name);
           }
 
           # save SNMP data for use by frontend
@@ -340,36 +263,17 @@ try {
 
           # run autoregistration
           if($cmd->autoreg()) {
-            tty_message("[$host] Running auto-registration (started)\n");
+            tty_message("[%s] Running auto-registration (started)\n", $hi->name);
             $hi->autoregister;
-            tty_message("[$host] Running auto-registration (finished)\n");
+            tty_message("[%s] Running auto-registration (finished)\n", $hi->name);
           }
-        }
 
-        catch ($err) {
-          chomp $err;
-          tty_message("[$host] Host poll failed ($err)\n");
         }
+      }
 
-      } # host processing block ends here
-
-      # getting arptable
-      elsif($task->[0] eq 'arp') {
-        tty_message("[arptable] Updating arp table (started)\n");
-        my $arptable = snmp_get_arptable(
-          $cfg->arpservers(), $cfg->snmp_community,
-          sub {
-            tty_message("[arptable] Retrieved arp table from $_[0]\n");
-          }
-        );
-        if(!ref $arptable) {
-          tty_message("[arptable] Updating arp table (failed, $arptable)\n");
-        } else {
-          tty_message("[arptable] Updating arp table (processing)\n");
-          my $e = sql_arptable_update($arptable);
-          if($e) { tty_message("[arptable] Updating arp table (failed, $e)\n"); }
-          else { tty_message("[arptable] Updating arp table (finished)\n"); }
-        }
+      catch ($err) {
+        chomp $err;
+        tty_message("[%s] Host poll failed ($err)\n", $hi->name);
       }
 
 	    # child finish
@@ -384,10 +288,7 @@ try {
   while(($cpid = wait()) != -1) {
     $tasks_cur--;
     my $ctask = clear_task_by_pid(\@work_list, $cpid);
-    tty_message(
-      "[main] Child %s reaped\n",
-      $ctask->[0] eq 'host' ? $ctask->[1] : 'arptable'
-    );
+    tty_message("[main] Child %s reaped\n", $ctask->[0]->name);
     tty_message("[main] $tasks_cur children remaining\n");
   }
   die "Assertion failed! \$tasks_cur non-zero." if $tasks_cur;
