245af9feec04430735ffce0ba14ca548e1b0dbdc
Borek Lupomesky <borek.lupomesky@vodafone.com>
2023-02-12 16:41:31 +0100

    Convert Model::SNMPDbTable::save to using Mojo::Pg
    This conversion require somewhat extensive rewrite, hopefully improving
    the code.

--- lib/SPAM/Model/SNMPDbTable.pm
+++ lib/SPAM/Model/SNMPDbTable.pm
@@ -9,6 +9,7 @@ use experimental 'signatures', 'postderef';
 
 use Carp;
 use Feature::Compat::Try;
+use Data::Dumper;
 use SPAM::Config;
 use SPAM::DbTransaction;
 use SPAM::Misc qw(hash_create_index hash_iterator hash_index_access);
@@ -39,7 +40,7 @@ has _debug_fh => ( is => 'rw' );
 sub _build__db ($self)
 {
   my $db = SPAM::Config->instance->get_mojopg_handle('spam')->db;
-  my $ref_time = time;
+  my $now = time;
   my $table = 'snmp_' . lc $self->obj->name;
   my @object_index = $self->obj->index->@*;
   my %data;
@@ -47,7 +48,7 @@ sub _build__db ($self)
   my $r = $db->select($table,
     [
       \'*',
-      \"$ref_time - extract(epoch from date_trunc('second', chg_when)) AS chg_age"
+      \"$now - extract(epoch from date_trunc('second', chg_when)) AS chg_age"
     ],
     { host => $self->host->name }
   );
@@ -93,33 +94,57 @@ sub _dbg ($self, $fmt=undef, @args)
 }
 
 #-------------------------------------------------------------------------------
+# internal code to drop old entries, this is configured by 'dbmaxage' key
+# in MIB object configuration
+sub _delete_old_entries ($self, $tx)
+{
+  my $count = 0;
+  # do nothing if dbmaxage is not defined (which means we just leave old
+  # entries in db)
+  return 0 unless defined $self->obj->dbmaxage;
+
+  hash_iterator($self->_db, scalar($self->obj->index->@*), sub ($leaf, @path) {
+
+    # do nothing if the entry is not old enough
+    return unless $leaf->{chg_age} > $self->obj->dbmaxage;
+
+    # where clause links indices to their values
+    my %where_clause = ( host => $self->obj->name );
+    for my $i (0 .. $#path) {
+      $where_clause{lc $self->obj->index->[$i]} = $path[$i];
+    }
+
+    $tx->delete('snmp_' . lc $self->obj->name, \%where_clause);
+    $count++;
+  });
+
+  return $count;
+}
+
+#-------------------------------------------------------------------------------
 # save current SNMP object into database
 sub save ($self)
 {
   # other variables
   my $host = $self->host;
-  my $snmp_object = $self->obj;
-  my $cfg = SPAM::Config->instance;
-  my $dbh = $cfg->get_dbi_handle('spam');
+  my $db = SPAM::Config->instance->get_mojopg_handle('spam')->db;
   my %stats = ( insert => 0, update => 0, delete => 0 );
-  my $ref_time = time();   # reference 'now' point of time
-  my $tx = SPAM::DbTransaction->new;
+  my $now = time;
 
-  $self->_dbg('REFERENCE TIME: %s', scalar(localtime($ref_time)));
+  $self->_dbg('REFERENCE TIME: %s', scalar(localtime($now)));
 
   try {
 
     # ensure database connection
-    die 'Cannot connect to database (spam)' unless ref $dbh;
+    die 'Cannot connect to database (spam)' unless ref $db;
 
-    # find the MIB object we're saving
-    my $obj = $cfg->find_object($snmp_object->name);
-    my @object_index = @{$snmp_object->index};
+    # get the MIB object's index
+    my @object_index = $self->obj->index->@*;
     $self->_dbg('OBJECT INDEX: %s', join(', ', @object_index));
 
     # find the object in the host instance
-    my $object = $host->snmp->get_object($snmp_object->name);
-    die "Object $snmp_object does not exist" unless $object;
+    my $object = $host->snmp->get_object($self->obj->name);
+    die sprintf(q{Object '%s' not loaded}, $self->obj->name) unless $object;
 
     # debugging output
     if($self->_db->%*) {
@@ -128,121 +153,49 @@ sub save ($self)
       $self->_dbg('CURRENT ROWS DUMP END');
     }
 
-    # collect update plan; there are three conceptual steps:
-    # 1. entries that do not exist in %old (= loaded from database) will be
-    #    inserted as new
-    # 2. entries that do exist in %old will be updated in place
-    # 3. entries that do exist in %old but not in $object (= retrieved via
-    #    SNMP) will be deleted
-    $tx->add(
-      sprintf(
-        'UPDATE snmp_%s SET fresh = false WHERE host = ?',
-        $snmp_object->name
-      ),
-      $host->name
-    );
+    $db->txn(sub ($tx) {
 
-    # iterate over the SNMP-loaded data
-    hash_iterator(
-      $object,
-      scalar(@object_index),
-      sub {
-        my $leaf = shift;
-        my @idx = @_;
-        my $val_old = hash_index_access($self->_db, @idx);
-        my (@fields, @values, $query, @cond);
+      # clear the 'fresh' flag on all entries
+      my $table = 'snmp_' . lc $self->obj->name;
+      $tx->update($table, { fresh => 'f' }, { host => $self->obj->name });
 
-        # UPDATE - note, that we are not actually checking, if the data changed;
-        # just existence of the same (host, @index) will cause all columns to be
-        # overwritten with new values and 'chg_when' field updated
-        if($val_old) {
+      # commence iteration
+      $host->snmp->iterate_data($self->obj->name, sub ($where, $set, @idx) {
+        my $old_value = hash_index_access($self->_db, @idx);
+
+        # update
+        if($old_value) {
+          $tx->update($table, $set, { host => $self->obj->name, %$where });
           $stats{'update'}++;
-          $tx->add(
-            sprintf(
-              'UPDATE snmp_%s SET %s WHERE %s',
-              $snmp_object->name,
-              join(',', (
-                'chg_when = current_timestamp',
-                'fresh = true',
-                map { "$_ = ?" } @{$snmp_object->columns}
-              )),
-              join(' AND ', map { "$_ = ?" } ('host', @object_index))
-            ),
-            ( map {
-              $leaf->{$_}{'enum'} // $leaf->{$_}{'value'} // undef
-            } @{$snmp_object->columns} ),
-            $host->name, @idx,
-          );
-
           # set the age of the entry to zero, so it's not selected for deletion
-          $val_old->{'chg_age'} = 0;
+          $old_value->{'chg_age'} = 0;
         }
 
-        # INSERT
+        # insert
         else {
+          $tx->insert($table, { host => $host->name, %$set, %$where });
           $stats{'insert'}++;
-          $tx->add(
-            sprintf(
-              'INSERT INTO snmp_%s ( %s ) VALUES ( %s )',
-              $snmp_object->name,
-              join(',',
-                ('host', 'fresh', @object_index, @{$snmp_object->columns})
-              ),
-              join(',',
-                ('?') x (2 + @object_index + @{$snmp_object->columns})
-              ),
-            ),
-            $host->name, 't', @idx,
-            map {
-              $leaf->{$_}{'enum'} // $leaf->{$_}{'value'} // undef
-            } @{$snmp_object->columns}
-          );
         }
-      }
-    );
+      });
 
-    # DELETE
-    my $dbmaxage = $snmp_object->dbmaxage // undef;
-    if(defined $dbmaxage) {
-      hash_iterator(
-        $self->_db,
-        scalar(@object_index),
-        sub {
-          my $leaf = shift;
-          my @idx = splice @_, 0;
+      # delete, if 'dbmaxage' is defined for a MIB object, deleting of old
+      # entries is performed
+      $stats{'delete'} += $self->_delete_old_entries($tx);
 
-          if($leaf->{'chg_age'} > $dbmaxage) {
-            $stats{'delete'}++;
-            $tx->add(
-              sprintf(
-                'DELETE FROM snmp_%s WHERE %s',
-                $snmp_object->name,
-                join(' AND ', map { "$_ = ?" } ('host', @{$snmp_object->index}))
-              ),
-              $host->name, @idx
-            );
-          }
-        }
-      );
-    }
+    }); # transaction ends here
 
     # debug output
     $self->_dbg(
-      'UPDATE PLAN INFO (%d rows, %d inserts, %d updates, %d deletes)',
-      $tx->count, @stats{'insert','update', 'delete'}
+      'TRANSACTION DONE (%d inserts, %d updates, %d deletes)',
+      @stats{'insert','update', 'delete'}
     );
-
-    # perform database transaction
-    if($tx->count) {
-      my $e = $tx->commit;
-      die $e if $e;
-    }
 
   }
 
   catch ($err) {
+    chomp $err;
     $self->_dbg('EXCEPTION: %s', $err);
-    #die $err;
+    die $err;
   }
 
   # finish
